8f7ff8a670076dfca3d48e422cb69d37034a4aa0
elenaniksy <elfhelen@gmail.com>
2020-10-11 22:06:27 +0300

    feat: add solution for bracket task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+
+    function isOpen (openBracket, topStack) {
+        for(let i = 0; i < bracketsConfig.length; i++) {
+            if(openBracket === bracketsConfig[i][0]) {
+                if(bracketsConfig[i][0] === bracketsConfig[i][1]) {
+                    if(topStack === openBracket) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function getClose(openBracket) {
+        for(let i = 0; i < bracketsConfig.length; i++) {
+            if(openBracket === bracketsConfig[i][0]){
+                return bracketsConfig[i][1];
+            }
+        }
+    }
+
+    for(let i = 0; i < str.length; i++) {
+        let bracket = str[i];
+        if(isOpen(bracket, stack[stack.length-1])) {
+            stack.push(bracket);
+        } else {
+            let openBracket = stack.pop();
+            let closeBracket = getClose(openBracket);
+            if(bracket !== closeBracket) {
+                return false;
+            }
+        }
+    }
+
+    if (stack.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
